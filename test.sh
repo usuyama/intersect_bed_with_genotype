2908210d0be7a91345413ad6b702329b687c42be
Usuyama_Naoto <usuyama@gw12.(none)>
2013-08-12 19:02:21 +0900

    test script

---
+++ test.sh
@@ -0,0 +1,4 @@
+ruby intersect_bed_g.rb -a test/1.bed -b test/2.bed -v
+ruby intersect_bed_g.rb -a test/1.bed -b test/2.bed -v -wb
+ruby intersect_bed_g.rb -a test/1.bed -b test/2.bed
+ruby intersect_bed_g.rb -a test/1.bed -b test/2.bed -sorted
